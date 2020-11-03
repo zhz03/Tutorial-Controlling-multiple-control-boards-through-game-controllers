836b755816f3015ab25cb17b7fdf8d8b2f6b2776
Zhaoliang <zhz03@g.ucla.edu>
2020-11-03 00:53:39 -0600

    added the Check MAC program for esp8266

---
+++ MAC_esp8266/MAC_esp8266.ino
@@ -0,0 +1,15 @@
+#include <ESP8266WiFi.h>
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("ESP8266 Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+}
+ 
+void loop(){
+  Serial.println();
+  Serial.print("ESP8266 Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+  delay(1000);
+}
