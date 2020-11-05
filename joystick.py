14f454d8d06f821e9f3a021f7e34aa2054444fd1
Parth Agrawal <parth@Parths-MacBook-Pro.local>
2020-11-05 07:19:32 +0530

    Vertical control and PWM added

--- joystick.py
+++ joystick.py
@@ -5,14 +5,18 @@ pygame.init()
 pygame.joystick.init()
 done = False
 
-serial_port = serial.Serial("COM9",115200)
+PORT = "COM9"
+
+serial_port = serial.Serial(PORT,115200)
 serial_port.close()
 serial_port.open()
 
-press_prev = 0 
 control = 0
-x = 0
-y = 0
+PWM = 0
+x_horizontal = 0
+y_horizontal = 0
+x_vertical = 0
+y_vertical = 0
 
 
 while not done:
@@ -23,24 +27,37 @@ while not done:
 	joystick = pygame.joystick.Joystick(0)
 	joystick.init()
 	axes = joystick.get_numaxes()
-	x_curr = round(joystick.get_axis(0))
-	y_curr = round(joystick.get_axis(1))
-	control_press = joystick.get_button(5)
-	if x != x_curr or y != y_curr or (control_press == 0 and press_prev == 1):
-		x = x_curr
-		y = y_curr
-		if press_prev:
-			control = (control + 1) % 3
-		value = str(x+1) + str(y+1) + str(control)
+	x_horizontal_curr = round(joystick.get_axis(0))
+	y_horizontal_curr = round(joystick.get_axis(1))
+
+	x_vertical_curr = round(0)
+	y_vertical_curr = round(0)
+
+	control_press_A = joystick.get_button(0)
+	control_press_B = joystick.get_button(1)
+	control_press_X = joystick.get_button(2)
+	control_press_Y = joystick.get_button(3)
+	control_val = control_press_A * 1 + control_press_B * 2 + control_press_X * 3 + control_press_Y * 4
+
+	PWM_L2_R2 = round(joystick.get_axis(2))
+	PWM_L1 = joystick.get_button(4)
+	PWM_R1 = joystick.get_button(5)
+	PWM_val = 1 if PWM_L2_R2 < 0 else PWM_L2_R2 * 2 + PWM_L1 * 3 + PWM_R1 * 4
+
+	if x_horizontal != x_horizontal_curr or y_horizontal != y_horizontal_curr or x_vertical != x_vertical_curr or y_vertical != y_vertical_curr or (PWM_val != PWM and PWM_val != 0) or (control_val != 0 and control_val != control):
+		x_horizontal = x_horizontal_curr
+		y_horizontal = y_horizontal_curr
+		x_vertical = x_vertical_curr
+		y_vertical = y_vertical_curr
+		control = control_val if control_val != 0 else control
+		PWM = PWM_val if PWM_val != 0 else PWM
+		
+		value = str(x_horizontal+1) + str(y_horizontal+1) + str(x_vertical+1) + str(y_vertical+1) + str(control) + str(PWM)
 		serial_port.write(value.encode())
-		#serial_port.write(str(y + 1).encode())
-		#serial_port.write(str(control).encode())
-		#print(len(str(x+1).encode()))
-		#print(len(str(x+1).encode()))
-		#print(len(str(control).encode()))
-		print("value: {}".format(value))
-		print("X: {}".format(str(x + 1).encode()))
-		#print("Y: {}".format(str(y + 1).encode()))
-		#print("Control: {}".format(str(control).encode()))
-	press_prev = control_press
+		print("X_horizontal : {}".format(str(x_horizontal+1).encode()))
+		print("Y_horizontal : {}".format(str(y_horizontal+1).encode()))
+		print("X_vertical : {}".format(str(x_vertical+1).encode()))
+		print("Y_vertical : {}".format(str(y_vertical+1).encode()))
+		print("Control : {}".format(str(control).encode()))
+		print("PWM : {}".format(str(PWM).encode()))
 pygame.quit()
